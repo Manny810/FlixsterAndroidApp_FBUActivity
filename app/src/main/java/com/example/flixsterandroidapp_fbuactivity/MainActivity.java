551ffe02c9e70d4e336d7f60487608d27e448467
Manny810 <mannyfavela2000@gmail.com>
2020-06-24 13:01:04 -0500

    implemented caller to movie database api

--- app/src/main/java/com/example/flixsterandroidapp_fbuactivity/MainActivity.java
+++ app/src/main/java/com/example/flixsterandroidapp_fbuactivity/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.flixsterandroidapp_fbuactivity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+
+import okhttp3.Headers;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=557bf444fa647aa33e0e1a2de0317f55";
+    public static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(NOW_PLAYING_URL, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                Log.d(TAG, "OnSuccess");
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                Log.d(TAG, "OnFailure");
+            }
+        });
     }
 }
